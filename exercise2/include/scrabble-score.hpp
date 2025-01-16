8c9fef7679e75b8b9656906796b21f4a6728a00f
Adam Dziwiszek <adziwiszek1@gmail.com>
2025-01-16 18:25:11 +0100

    added exercise2

---
+++ exercise2/include/scrabble-score.hpp
@@ -0,0 +1,10 @@
+#include<exception>
+
+/*class DayOutOfBoundsException : std::exception {
+public:
+    char* what(){
+        return "Day out of bounds";
+    }
+};*/
+
+ int dayOfYear(int month, int dayOfMonth, int year);
---
+++ exercise2/ut/scrabble-score-tests.cpp
@@ -0,0 +1,27 @@
+#include "gtest/gtest.h"
+#include "day-of-year.hpp"
+
+struct DayOfYearTestSuite {};
+
+TEST(DayOfYearTestSuite, February29LeapYear)
+{
+  ASSERT_EQ(dayOfYear(2, 29, 2024), 31 + 29);
+}
+
+TEST(DayOfYearTestSuite, January1stIsFitstDayOfYear)
+{
+  ASSERT_EQ(dayOfYear(1, 1, 2020), 1);
+}
+
+/*TEST(Exception, DayOutOfBoundsException)
+{
+  EXPECT_THROW({
+    try{
+      dayOfYear(1, 421, 2020);
+    }
+    catch(const DayOutOfBoundsException& e) {
+      EXPECT_STREQ("Day out of bounds", e.what());
+      throw;
+    }
+  }, DayOutOfBoundsException);
+}*/
